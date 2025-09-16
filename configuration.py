f2be45faa989f3629998bc2cab8b33fbbc0577dc
YuliaZaiduk <zajduk65@mail.ru>
2025-09-16 22:26:50 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,4 @@
+# Зайдук Юлия, 34-я когорта - Финальный проект. Инженер по тестированию плюс
+URL_SERVICE = "https://24596657-ee13-438e-800f-1cf88202c06f.serverhub.praktikum-services.ru/"
+CREATE_ORDER = "/api/v1/orders"
+GET_ORDER_TRACK = "/api/v1/orders/track"
